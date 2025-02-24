3b361064c28918eb2285f761e00cd5e3e87ebeaa
saneyoshi <saneyoshi@collabos.com>
2025-02-24 21:43:12 +0900

    add:sqlコードの更新

--- backend/sql/queries/tasks.sql
+++ backend/sql/queries/tasks.sql
@@ -1,11 +1,12 @@
 -- sql/queries/tasks.sql
 
 -- name: CreateTask :exec
-INSERT INTO tasks (id, title, description, is_completed, user_id)
-VALUES (?, ?, ?, ?, ?);
+INSERT INTO tasks (id, title, description, is_completed, user_id, assignee_id, priority, due_date)
+VALUES (?, ?, ?, ?, ?, ?, ?, ?);
 
 -- name: UpdateTask :exec
-UPDATE tasks SET title = ?, description = ?, is_completed = ? WHERE id = ?;
+UPDATE tasks SET title = ?, description = ?, is_completed = ?, assignee_id = ?, priority = ?, due_date = ?
+WHERE id = ?;
 
 -- name: ListTasks :many
 SELECT * FROM tasks WHERE user_id = ? ORDER BY created_at DESC;
